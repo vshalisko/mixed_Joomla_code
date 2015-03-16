0821fd8ba4ed1f5fb58cc9a2305b2603e7f81405
vshalisko <vshalisko@gmail.com>
2015-03-15 23:50:05 -0600

    Minor changes

--- mod_qlform_templates/ajt005_j30_html_mod_qlform/prefilled_helper.php
+++ mod_qlform_templates/ajt005_j30_html_mod_qlform/prefilled_helper.php
@@ -17,7 +17,7 @@ if (isset($_POST)) foreach ($_POST as $k=>$v):
 	$dataToBind->$k=$v;
 endforeach;
 
-/* taking values from database to fill some fields in table with data from database (external) */
+// taking values from parcel database to fill some fields in table with data
 if (1==$params->get('todoDatabaseExternal') AND 1==$checkDatabaseExternal
 	AND isset($dataToBind->parcel_map_id) AND isset($dataToBind->parcel_map_version_id))
 {
@@ -33,13 +33,60 @@ if (1==$params->get('todoDatabaseExternal') AND 1==$checkDatabaseExternal
 		if (isset($my_db1_result->parcel_map_properties_xml)) $dataToBind->case_parcel_properties_xml = $my_db1_result->parcel_map_properties_xml;			
 		}
 }
-/* $temporal_identifier - the temporal solution */
+// $temporal_identifier - the temporal solution 
 $temporal_identifier = 'M' . $dataToBind->parcel_map_id . 'C' . $dataToBind->parcel_id . 'XXXXXXXX';
 $dataToBind->official_case_identifier=$temporal_identifier;
 
-echo '<pre>Datos del predio:';
-echo $dataToBind->case_parcel_properties_xml;
-echo '</pre>';
+if (isset($dataToBind->case_parcel_properties_xml)) {
+	echo '<pre>Datos del predio:<br />';
+	echo $dataToBind->case_parcel_properties_xml;
+	echo '</pre>';
+} else {
+	JFactory::getApplication()->enqueueMessage(JText::_(
+	'<H4>Aviso: Forma no valida! El predio con las caracteristicas especificados no fue encontrado en sistema.</H4>'
+	), 'warning');
+}
+
+// getting data from Joomla to fill user related fields
+$joomla_user = JFactory::getUser();
+if (!$joomla_user->guest) {
+	// user logged in
+	$dataToBind->person_login = $joomla_user->username;
+
+	// checking presence of user personal data in database and setting values from filled form in such a case
+	if (1==$params->get('todoDatabaseExternal') AND 1==$checkDatabaseExternal)
+	{
+		$my_db1=JDatabaseDriver::getInstance($paramsDatabaseExternal);
+	        $my_db1->setQuery('SELECT person_id, person_name, person_email, person_curp
+			FROM persons WHERE person_login = '. $my_db1->quote($dataToBind->person_login));
+		$my_db1->execute();
+                if( $my_db1->getNumRows() > 0) {
+			// the database do has record for this user login
+			$my_db1_result=$my_db1->loadObject();
+			if (isset($my_db1_result->person_id)) $dataToBind->person_id = $my_db1_result->person_id;
+			if (isset($my_db1_result->person_name)) $dataToBind->person_name = $my_db1_result->person_name;
+			if (isset($my_db1_result->person_email)) $dataToBind->person_email = $my_db1_result->person_email;
+			if (isset($my_db1_result->person_curp)) $dataToBind->person_curp = $my_db1_result->person_curp;
+		}
+	}
+} else {
+	JFactory::getApplication()->enqueueMessage(JText::_(
+	'<H4>Aviso: Forma no valida! Usted tiene que entrar al sistema para iniciar el tramite.</H4>'
+	), 'warning');
+}
+
+if (isset($dataToBind->person_name)) {
+	echo '<pre>Nombre del promotor:<br />';
+	echo $dataToBind->person_name;
+	echo '</pre>';
+} else {
+	JFactory::getApplication()->enqueueMessage(JText::_(
+	'<H4>Aviso: Forma no valida! Usted tiene que tener registrados sus datos personales.</H4>'
+	), 'warning');
+
+}
+
+
 
 $form->bind($dataToBind);
 
